573c2f00f0dbe5bb0156093b27aee8d0ed309c66
cameronmcnz <you@example.com>
2025-07-07 17:43:34 +0300

    Created Navigation for the all screens

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,19 +5,13 @@ plugins {
 }
 
 android {
-<<<<<<< HEAD
-    namespace = "com.alpha.mosaic"
-    compileSdk = 36
 
-    defaultConfig {
-        applicationId = "com.alpha.mosaic"
-=======
     namespace = "com.mosaicapplication.mosaicapp"
     compileSdk = 36
 
     defaultConfig {
         applicationId = "com.mosaicapplication.mosaicapp"
->>>>>>> 880b41676385f96f4d673b6e8ce1c84d1b731c94
+
         minSdk = 24
         targetSdk = 36
         versionCode = 1
@@ -45,19 +39,14 @@ android {
     buildFeatures {
         compose = true
     }
-<<<<<<< HEAD
+
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.14" // Compatible with Compose 1.6.x
     }
 }
 
 dependencies {
-=======
-}
 
-dependencies {
-
->>>>>>> 880b41676385f96f4d673b6e8ce1c84d1b731c94
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -66,10 +55,9 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-<<<<<<< HEAD
-    implementation("androidx.navigation:navigation-compose:2.7.7") // Use stable version
-=======
->>>>>>> 880b41676385f96f4d673b6e8ce1c84d1b731c94
+
+    implementation(libs.androidx.navigation.compose) // Use stable version
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -77,8 +65,5 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-<<<<<<< HEAD
-=======
-    implementation("androidx.navigation:navigation-compose:2.7.5")
->>>>>>> 880b41676385f96f4d673b6e8ce1c84d1b731c94
+
 }
--- app/src/main/java/com/mosaicapplication/mosaicapp/navigation.kt
+++ app/src/main/java/com/mosaicapplication/mosaicapp/navigation.kt
@@ -1,9 +1,16 @@
-package com.alpha.mosaic
+package com.mosaicapplication.mosaicapp
+
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.mosaicapplication.mosaicapp.ui.screens.Authentication
+import com.mosaicapplication.mosaicapp.ui.screens.ForgotPasswordScreen
+import com.mosaicapplication.mosaicapp.ui.screens.ResetPassword
+import com.mosaicapplication.mosaicapp.ui.screens.SignUp
+import com.mosaicapplication.mosaicapp.ui.screens.TermsAndConditions
+import com.mosaicapplication.mosaicapp.ui.screens.VerifyCode
 
 
 @Composable
